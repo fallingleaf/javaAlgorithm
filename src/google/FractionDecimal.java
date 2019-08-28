06174b9397ff46cbe581621b149b2b5e12306afb
Tam Nguyen <tamnghoang@gmail.com>
2019-08-28 16:07:25 -0700

    Push comment

--- src/google/FractionDecimal.java
+++ src/google/FractionDecimal.java
@@ -5,14 +5,16 @@ import java.util.*;
 // https://leetcode.com/problems/fraction-to-recurring-decimal/submissions/
 // Find recurring value of fraction
 // Cases: numerator is 0, negative number, overflow
-class FractionDecimal {
+// Use hashmap to store remain value with index,
+// everytime check if new value exists
+public class FractionDecimal {
 
     public String fractionToDecimal(int numerator, int denominator) {
         if(numerator == 0) {
             return "0";
         }
         long num = Math.abs((long)numerator), div = Math.abs((long)denominator);
-        String sign = (numerator > 0) ^ (denominator > 0) ? "-" : "";
+        String sign = (numerator > 0) == (denominator > 0) ? "" : "-";
 
         HashMap<Long, Integer> hm = new HashMap<> ();
         long val = num/div;
